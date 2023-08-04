418617d364e55e248e837c696793eb52fc81a4a4
Jesus M Villar <j.villarvazquez@celonis.com>
2023-08-04 08:30:50 +0200

    initial commit

---
+++ src/main/java/es/geeksusma/passwordvalidator/PasswordValidatorApplication.java
@@ -0,0 +1,13 @@
+package es.geeksusma.passwordvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/geeksusma/passwordvalidator/PasswordValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package es.geeksusma.passwordvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
